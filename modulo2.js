598c4b6b28150be261fc932f3e33cad7daccd012
Renan Schmitt <renan.schmitt@dbserver.com.br>
2020-03-05 11:38:31 -0300

    First Commit

---
+++ modulo2.js
@@ -0,0 +1,5 @@
+const modulo2 = {
+    fun1: (a,b,c) => a+b+c
+}
+
+module.exports = modulo2
---
+++ modulo2.test.js
@@ -0,0 +1,8 @@
+const modulo2 = require('./modulo2')
+
+describe('utilizando sózinho :D ', () => {
+    it('Testando a soma dos parametros', () => {
+        expect(modulo2.fun1(5, 10, 20)).toBe(35)
+    })
+})
+
